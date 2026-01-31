b11aeb8a500d47d11e52c0385e40206960f832c2
Arthur Lazarenko <arthur.lazarenko@icloud.com>
2026-01-31 14:24:25 +0200

    initialize repository

--- src/config.ts
+++ src/config.ts
@@ -13,13 +13,13 @@ export const PRISMA_OPTIONS: PrismaClientOptions = {
     min: Math.min(os.availableParallelism() * 1, 10),
     max: Math.min(os.availableParallelism() * 2 + 1, 20),
 
-    idle_in_transaction_session_timeout: 30 * 1000,
+    idle_in_transaction_session_timeout: 30_000,
     options: '-c random_page_cost=1.1 -c seq_page_cost=1.0 -c work_mem=64MB -c temp_buffers=16MB',
   }),
 
   log: ['info', 'warn', 'error'],
   omit: { user: { password: true } },
-  transactionOptions: { maxWait: 2_000, timeout: 10_000 },
+  transactionOptions: { maxWait: 5_000, timeout: 10_000 },
 };
 
 export const EXTENSION_OPTIONS: Parameters<typeof speedExtension>[0] = {
